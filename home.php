bcc684647a10de59d16773b1102e1e697a835666
Suhas Pete <petesuhas@gmail.com>
2023-12-20 16:43:50 +0530

    added deluxroom

--- home.php
+++ home.php
@@ -19,6 +19,9 @@
     <!-- sweet alert -->
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <link rel="stylesheet" href="./admin/css/roombook.css">
+    <!-- CSS Files -->
+<link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
+<link rel="stylesheet" href="https://unpkg.com/bs-brain@2.0.3/components/abouts/about-1/assets/css/about-1.css" />
 
     <style>
     
@@ -36,6 +39,17 @@
       width: 18%;
 
       } 
+     
+    .logo{
+        text-align: center !important;
+    }
+    .logo p
+    {
+font-size: 17px !important;
+    }
+    
+
+
       }
 
 
@@ -276,7 +290,9 @@
               <i class="fa-solid fa-spa"></i>
               <i class="fa-solid fa-dumbbell"></i>
             </div>
-            <button class="btn btn-primary bookbtn" onclick="openbookbox()">Book</button>
+            <!-- <button class="btn btn-primary bookbtn" onclick="openbookbox()">Book</button> -->
+            <button class="btn btn-primary bookbtn" onclick="redirectToDeluxRoom()">Book</button>
+
           </div>
         </div>
         <div class="roombox">
@@ -328,6 +344,151 @@
   </section>
 
   <hr>
+
+
+<!-- About 1 - Bootstrap Brain Component -->
+<section class="py-3 py-md-5 py-xl-8">
+  <div class="container">
+    <div class="row gy-3 gy-md-4 gy-lg-0 align-items-lg-center">
+      <div class="col-12 col-lg-6 col-xl-5">
+        <img class="img-fluid rounded" loading="lazy" src="image\about-img-1.jpeg" alt="">
+      </div>
+      <div class="col-12 col-lg-6 col-xl-7">
+        <div class="row justify-content-xl-center">
+          <div class="col-12 col-xl-11">
+            <h2 class="h1 mb-3">Who Are We?</h2>
+            <p class="lead fs-4 text-secondary mb-3">We help people to build incredible brands and superior products. Our perspective is to furnish outstanding captivating services.</p>
+            <p class="mb-5">Nullam gravida orci ac luctus molestie. Fusce finibus congue erat, non aliquam magna tincidunt at. Aenean lacinia arcu ex, sed pharetra nibh porta a. Curabitur vel consequat nibh, ac interdum nisl. Nunc pulvinar nec massa vitae sollicitudin.</p>
+            <div class="row gy-4 gy-md-0 gx-xxl-5X">
+              <div class="col-12 col-md-6">
+                <div class="d-flex">
+                  <div class="me-4 text-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
+                      <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
+                    </svg>
+                  </div>
+                  <div>
+                    <h4 class="mb-3">Versatile Brand</h4>
+                    <p class="text-secondary mb-0">We are crafting a digital method that subsists life across all mediums.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 col-md-6">
+                <div class="d-flex">
+                  <div class="me-4 text-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-fire" viewBox="0 0 16 16">
+                      <path d="M8 16c3.314 0 6-2 6-5.5 0-1.5-.5-4-2.5-6 .25 1.5-1.25 2-1.25 2C11 4 9 .5 6 0c.357 2 .5 4-2 6-1.25 1-2 2.729-2 4.5C2 14 4.686 16 8 16Zm0-1c-1.657 0-3-1-3-2.75 0-.75.25-2 1.25-3C6.125 10 7 10.5 7 10.5c-.375-1.25.5-3.25 2-3.5-.179 1-.25 2 1 3 .625.5 1 1.364 1 2.25C11 14 9.657 15 8 15Z" />
+                    </svg>
+                  </div>
+                  <div>
+                    <h4 class="mb-3">Digital Agency</h4>
+                    <p class="text-secondary mb-0">We believe in innovation by merging primary with elaborate ideas.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
+<!-- <section class="u-align-center u-clearfix u-section-1" id="carousel_ec01">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h2 class="u-custom-font u-font-montserrat u-text u-text-default u-text-1"> Here are some of our clients</h2>
+        <div class="custom-expanded u-list u-list-1">
+          <div class="u-repeater u-repeater-1">
+            <div class="u-align-center u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-1">
+                <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-1" src="images/Untitled.png" alt="" data-image-width="225" data-image-height="225">
+              </div>
+            </div>
+            <div class="u-align-center u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-2">
+                <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-2" src="images/makemytrip.png" alt="" data-image-width="398" data-image-height="126">
+              </div>
+            </div>
+            <div class="u-align-center u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-3">
+                <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-3" src="image\airbnb.png" alt="" data-image-width="398" data-image-height="126">
+              </div>
+            </div>
+            <div class="u-align-center u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-4">
+                <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-4" src="image\makemytrip.png" alt="" data-image-width="225" data-image-height="225">
+              </div>
+            </div>
+            <div class="u-align-center u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-5">
+                <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-5" src="images/makemytrip.png" alt="" data-image-width="398" data-image-height="126">
+              </div>
+            </div>
+            <div class="u-align-center u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-6">
+                <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-6" src="images/black-logo-6.png" alt="" data-image-width="131" data-image-height="171">
+              </div>
+            </div>
+            <div class="u-align-center u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-7">
+                <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-7" src="images/black-logo-8_150.png" alt="" data-image-width="148" data-image-height="150">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section> -->
+    
+    <section class="u-align-center u-clearfix u-section-1" id="carousel_ec01">
+    <div class="u-clearfix u-sheet u-sheet-1">
+        <h2 class="u-custom-font u-font-montserrat u-text u-text-default u-text-1">Here are some of our clients</h2>
+        <div class="custom-expanded u-list u-list-1">
+            <div class="u-repeater u-repeater-1" style="display: flex; justify-content: center; align-items: center;">
+                <div class="u-align-center u-container-style u-list-item u-repeater-item" style="flex: 0 0 25%;">
+                    <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-1">
+                        <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-1" src="image\airbnb.png" alt="" data-image-width="225" data-image-height="225">
+                    </div>
+                </div>
+                <div></div>
+                <div class="u-align-center u-container-style u-list-item u-repeater-item" style="flex: 0 0 25%;">
+                    <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-2">
+                        <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-2" src="image\makemytrip.png" alt="" data-image-width="398" data-image-height="126">
+                    </div>
+                </div>
+                <div></div>
+                <div class="u-align-center u-container-style u-list-item u-repeater-item" style="flex: 0 0 25%;">
+                    <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-3">
+                        <img class="u-image u-image-contain u-image-default u-preserve-proportions u-image-3" src="image\airbnb.png" alt="" data-image-width="398" data-image-height="126">
+                    </div>
+                </div>
+                <!-- Add similar blocks for other images -->
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     <?php
@@ -498,6 +659,10 @@ openbookbox = () => {
     }
     closebox = () =>{
       bookbox.style.display = "none";
+    }
+
+    redirectToDeluxRoom = () => {
+        window.location.href = "http://localhost/hotel_project_/DeluxRoom.php";
     }
 </script>
 </html>
